42757c7bd95df4c17adaaccf9c8ccada15f6be7b
Morinosuke Tomihara <240344g@ugs.kochi-tech.ac.jp>
2023-06-21 13:09:41 +0900

    new logs

---
+++ fuel/app/logs/2023/06/05.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2023-06-05 11:58:42 --> shutdown - Session state failure transitioning from "closed" to "closed". in /Applications/MAMP/htdocs/AtriclePosting/fuel/core/classes/session/driver.php on 552
---
+++ fuel/app/logs/2023/06/07.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2023-06-07 05:03:29 --> shutdown - Session state failure transitioning from "closed" to "closed". in /Applications/MAMP/htdocs/AtriclePosting/fuel/core/classes/session/driver.php on 552
